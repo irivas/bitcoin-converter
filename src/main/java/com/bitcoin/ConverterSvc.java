0884dc95b3670caa1baec9d4141c53b2b76781a6
Israel Rivas <irvsglez@gmail.com>
2021-03-25 13:30:21 +0000

    First commit

---
+++ src/main/java/com/bitcoin/ConverterSvc.java
@@ -0,0 +1,68 @@
+package com.bitcoin;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.NumberFormat;
+import java.util.HashMap;
+
+public class ConverterSvc {
+  private static final HashMap<String, Integer> exchangeRates = new HashMap<String, Integer>();
+  static {
+    exchangeRates.put("USD", 100);
+    exchangeRates.put("GBP", 200);
+    exchangeRates.put("EUR", 300);
+  }
+
+  private final String BITCOIN_CURRENT_PRICE_URL = "https://api.coindesk.com/v1/bpi/currentprice.json";
+  private final HttpGet httpget = new HttpGet(BITCOIN_CURRENT_PRICE_URL);
+  private final CloseableHttpClient httpclient;
+  private CloseableHttpClient httpClient;
+
+
+  public ConverterSvc() {
+    this.httpclient = HttpClients.createDefault();
+  }
+  public ConverterSvc(CloseableHttpClient httpClient) {
+    this.httpclient = httpClient;
+  }
+
+  public double getExchangeRate(String currency) {
+    double rate = 0;
+
+    try {
+      CloseableHttpResponse response = this.httpclient.execute(httpget);
+      InputStream inputStream = response.getEntity().getContent();
+      var json = new BufferedReader(new InputStreamReader(inputStream));
+
+      @SuppressWarnings("deprecation")
+      JsonObject jsonObject = new JsonParser().parse(json).getAsJsonObject();
+      String n = jsonObject.get("bpi").getAsJsonObject().get(currency).getAsJsonObject().get("rate").getAsString();
+      NumberFormat nf = NumberFormat.getInstance();
+      rate = nf.parse(n).doubleValue();
+      System.out.println("El rate: ");
+      System.out.println(rate);
+
+
+    } catch (Exception ex) {
+      rate = -1;
+    }
+    return rate;
+  }
+
+  public double convertBitcoins(String currency, int coins) {
+    //return getExchangeRate(currency) * coins;
+    double dollars = 0;
+    var exchangeRate = getExchangeRate(currency);
+    dollars = exchangeRate * coins;
+
+    return dollars;
+  }
+}
---
+++ src/test/java/com/bitcoin/ConverterSvcTest.java
@@ -0,0 +1,78 @@
+package com.bitcoin;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.StatusLine;
+import org.mockito.Mockito;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.testng.MockitoTestNGListener;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class ConverterSvcTest {
+
+  private CloseableHttpClient client;
+  private CloseableHttpResponse response;
+  private StatusLine statusLine;
+  private HttpEntity entity;
+  private InputStream stream;
+
+  @BeforeTest
+  public void setUp() {
+    client = Mockito.mock(CloseableHttpClient.class);
+    response = Mockito.mock(CloseableHttpResponse.class);
+    statusLine = Mockito.mock(StatusLine.class);
+    entity = Mockito.mock(HttpEntity.class);
+
+    stream = new ByteArrayInputStream("{\"time\": {\"updated\": \"Oct 15, 2020 22:55:00 UTC\",\"updatedISO\": \"2020-10-15T22:55:00+00:00\",\"updateduk\": \"Oct 15, 2020 at 23:55 BST\"},\"chartName\": \"Bitcoin\",\"bpi\": {\"USD\": {\"code\": \"USD\",\"symbol\": \"&#36;\",\"rate\": \"11,486.5341\",\"description\": \"United States Dollar\",\"rate_float\": 11486.5341},\"GBP\": {\"code\": \"GBP\",\"symbol\": \"&pound;\",\"rate\": \"8,900.8693\",\"description\": \"British Pound Sterling\",\"rate_float\": 8900.8693},\"EUR\": {\"code\": \"EUR\",\"symbol\": \"&euro;\",\"rate\": \"9,809.3278\",\"description\": \"Euro\",\"rate_float\": 9809.3278}}}".getBytes());
+  }
+
+  @Test
+  public void getExchangeRateUSD() throws IOException {
+    //ConverterSvc converterSvc = new ConverterSvc();
+    Mockito.when(statusLine.getStatusCode()).thenReturn(200);
+    Mockito.when(response.getStatusLine()).thenReturn(statusLine);
+    Mockito.when(response.getEntity()).thenReturn(entity);
+    Mockito.when(entity.getContent()).thenReturn(stream);
+    Mockito.when(client.execute(Mockito.any(HttpGet.class))).thenReturn(response);
+
+    ConverterSvc converterSvc = new ConverterSvc(client);
+    var actual = converterSvc.getExchangeRate("USD");
+    double expected = 11486.5341;
+    Assert.assertEquals(actual, expected);
+  }
+
+  @Test(enabled=false)
+  public void getExchangeRateGBP() {
+    ConverterSvc converterSvc = new ConverterSvc();
+    var actual = converterSvc.getExchangeRate("GBP");
+    double expected = 200;
+    Assert.assertEquals(actual, expected);
+  }
+
+  @Test(enabled=false)
+  public void getExchangeRateEUR() {
+    ConverterSvc converterSvc = new ConverterSvc();
+    var actual = converterSvc.getExchangeRate("EUR");
+    double expected = 300;
+    Assert.assertEquals(actual, expected);
+  }
+
+  @Test(enabled=false)
+  public void convertBitcoins() {
+    ConverterSvc converterSvc = new ConverterSvc();
+    var actual = converterSvc.convertBitcoins("USD", 1);
+    double expected = 100;
+    Assert.assertEquals(actual, expected);
+  }
+}
